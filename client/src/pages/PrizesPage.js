6e2260f37c3a4be2450536d2e4ac6222d3129cb3
ml <m.liu48@lse.ac.uk>
2022-03-20 20:37:00 +0000

    add prize winners

--- client/src/pages/PrizesPage.js
+++ client/src/pages/PrizesPage.js
@@ -4,7 +4,16 @@ import PostComponent from '../components/PostComponent'
 import { Helmet } from 'react-helmet'
 import { API_URL } from '../config'
 
-const slugs = {'building-the-conscience-of-humankind': 'LSE Law Review Prize for Best Blog Post 2021', 'a-tale-of-communities': 'LSE Law Review Prize for Best Case Note 2021', 'treaties-norms-international-courts-hierarchy-theory-treading-water': 'LSE Law Review Prize for Best Blog Post 2020', 'r-evans-uneasy-precedent': 'Francis Taylor Building Prize for Best Case Note 2020', 'english-choice-law-contract-rome-i-regime': 'LSE Law Review Prize for Best Blog Post 2019' }
+const slugs = {
+    'fine-margin-examining-minority-majority-divide-enka-v-chubb': 'Freshfields Bruckhaus Deringer LLP Prize for Best Blog Post 2022',
+    'important-update-needed-international-human-rights-law-help-address-non-physical-harm-caused-military-cyberoperations': 'LSE Law Review Prize for Outstanding Blog Post 2022',
+    'rethinking-family-family-law-form-based-function-based-choice-based-frameworks': 'LSE Law Review Prize for Outstanding Blog Post 2022',
+    'building-the-conscience-of-humankind': 'LSE Law Review Prize for Best Blog Post 2021', 
+    'a-tale-of-communities': 'LSE Law Review Prize for Best Case Note 2021', 
+    'treaties-norms-international-courts-hierarchy-theory-treading-water': 'LSE Law Review Prize for Best Blog Post 2020', 
+    'r-evans-uneasy-precedent': 'Francis Taylor Building Prize for Best Case Note 2020', 
+    'english-choice-law-contract-rome-i-regime': 'LSE Law Review Prize for Best Blog Post 2019'
+}
 
 export default function PrizesPage() {
 
